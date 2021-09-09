bc8150da716af5eff0148fd6dba47509a5a636d3
kunxin-chor <chorkunxin@yahoo.com>
2021-09-09 03:37:10 +0000

    Add lab04 example

--- lab04-template-inheritance/index.js
+++ lab04-template-inheritance/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const wax = require('wax-on');
 
 /* 1. setup express */
 const app = express();
@@ -7,13 +8,24 @@ const app = express();
 /* 1b. setup view engine */
 app.set('view engine', 'hbs');
 
-/* 1c. inform Express where to find our static files */
+/* 1c. setup wax-on for hbs */
+wax.on(hbs.handlebars);
+// inform wax where to find our layouts file
+wax.setLayoutPath('./views/layouts');
+
+/* 1d. inform Express where to find our static files */
 app.use(express.static('public'))
 
 /* 2. Routes */
 /* associate an endpoint with a function */
 app.get('/', function(req,res){
     res.render('index')
+})
+
+app.get('/about-us', function(req,res){
+    res.render('about-us',{
+        "data":"whatever"
+    })
 })
 
 /* 3. Start server */
