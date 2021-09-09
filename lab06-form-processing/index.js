2ecb867d697daf75bc29f107c9e4a5f53f6b6010
kunxin-chor <chorkunxin@yahoo.com>
2021-09-09 06:43:41 +0000

    Added form processing example

---
+++ lab06-form-processing/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const hbs = require('hbs');
+const wax = require('wax-on');
+
+/* 1. setup express */
+const app = express();
+
+/* 1b. setup view engine */
+app.set('view engine', 'hbs');
+
+/* 1c. setup wax-on for hbs */
+wax.on(hbs.handlebars);
+// inform wax where to find our layouts file
+wax.setLayoutPath('./views/layouts');
+
+/* 1d. inform Express where to find our static files */
+app.use(express.static('public'))
+
+
+/* 2. Routes */
+/* associate an endpoint with a function */
+app.get('/', function(req,res){
+    res.render('index')
+})
+
+
+/* 3. Start server */
+app.listen(3000, function(){
+    console.log("Server started");
+})
